d1b62701be0187dcff374a1ca839818babbbe024
Diego Pires <dlpires@outlook.com>
2017-11-01 20:07:59 -0200

    Alteração servidor.cs

---
+++ chatServidor/Classes/Servidor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Collections;
+
+namespace chatServidor
+{
+    public class Servidor
+    {
+
+    }
+}
