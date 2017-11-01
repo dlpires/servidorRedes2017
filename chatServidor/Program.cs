2bf7d598495f30b12bbab5355f16538b74e5205c
JulaoBr <julioscp2009@hotmail.com>
2017-11-01 20:36:35 -0200

    V1.0

--- chatServidor/Program.cs
+++ chatServidor/Program.cs
@@ -16,7 +16,7 @@ namespace chatServidor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmServidor());
         }
     }
 }
