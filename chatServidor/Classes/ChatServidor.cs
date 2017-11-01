8719b44526cf9ca35669332b2a1c52539b6f7736
Diego Pires <dlpires@outlook.com>
2017-11-01 20:57:09 -0200

    Teste 2

--- chatServidor/Classes/ChatServidor.cs
+++ chatServidor/Classes/ChatServidor.cs
@@ -10,5 +10,16 @@ namespace chatServidor.Classes
     public class ChatServidor
     {
 
+        //consultado por usuário
+        public static Hashtable htUsuarios = new Hashtable(30); //Lista de Hash armazenando usuários(máx. 30)
+
+        //consultado por conexões
+        public static Hashtable htConexoes = new Hashtable(30); //Lista de Hash armazenando usuários(máx. 30)
+
+        private IPAddress enderecoIP; //guarda o endereço IP inserido;
+
+        private TcpClient tcpCliente;
+
+        public static event StatusChangedEventHandler StatusChanged;
     }
 }
